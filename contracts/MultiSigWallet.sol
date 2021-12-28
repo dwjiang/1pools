675d00f91e69aa146c64bb2c6094a9fdba0de79a
Kelly Wang <kellygnaw@gmail.com>
2021-12-27 20:02:04 -0800

    contract structure

---
+++ contracts/MultiSigWallet.sol
@@ -0,0 +1,29 @@
+contract MultiSigWallet {
+	function submitTransaction() {
+		/*
+			owner will propose a transaction subject to approval by other owners
+		*/
+
+	}
+
+	function confirmTransaction() {
+		/*
+			other owners can approve or deny the transaction proposal 
+		*/
+
+	}
+
+	function executeTransaction() {
+		/*
+			if enough owners approve the transaction, it will be executed 
+		*/
+
+	}
+
+	function revokeConfirmation() {
+		/*
+			allow an owner to cancel the confirmation  
+		*/
+
+	}
+}

7281d2938ca8e3ffe870203536ddc06e8e50ac5f
dwjiang <danieljiang1999@gmail.com>
2022-01-10 22:51:59 -0800

    verify if address is owner for creating updates

--- server/src/services/updatesService.js
+++ server/src/services/updatesService.js
@@ -1,10 +1,13 @@
 const updatesModel = require("../models/updates");
+const utils = require("../utils/utils");
 
 const findAll = async (pool) => {
   return await updatesModel.findAll({ where: { pool }, raw: true });
 }
 
 const create = async (pool, address, message) => {
+  if (!(await utils.verifyOwner(address, pool)))
+    throw new Error("Address is not an owner of this pool");
   return await updatesModel.create({
     pool, address, content: message
   });
